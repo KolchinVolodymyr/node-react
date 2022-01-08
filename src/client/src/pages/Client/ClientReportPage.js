1ebf3fa28c75180e88090f6c34fbd9dd1fe26fc4
Volodymyr Kolchin <KolchinVolo@gmail.com>
2022-01-09 01:18:54 +0200

    fix bug

--- src/client/src/pages/Client/ClientReportPage.js
+++ src/client/src/pages/Client/ClientReportPage.js
@@ -6,15 +6,34 @@ export const ClientReportPage = () => {
 //    let history = useHistory();
     const ID = useParams().id;
     const [data, setData] = useState([]);
+    const [month, setMonth ] = useState([]);
+    const [currentMonth, setCurrentMonth] = useState([]);
     const [employees, setEmployees] = useState([]);
+    const [employeesRender, setEmployeesRender] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
-    const total = 0;
-//    const [total, setTotal] = useState([]);
+
     const {request} = useHttp();
     const loadMessage = async () => {
         try {
             const response = await request(`/client/${ID}/report`, 'GET')
-            console.log('response', response);
+            // console.log('response', response.employeesItem);
+            response.employeesItem.forEach(el =>{
+                month.push(el.job.startDate);
+            })
+            // let newEmployees = [];
+            // response.employeesItem.forEach((a) => {
+            //     console.log('a forEach', a);
+            //         if('2021-12-12' === a.job.startDate) {
+            //             console.log('IF a.job.startDate', a.job);
+            //             newEmployees.push(a);
+            //             setEmployees(newEmployees);
+            //             // console.log('newEmployees', newEmployees);
+            //         }
+            //     // console.log('111 newEmployees', newEmployees);
+            // })
+            // console.log('12employees', employees);
+            // setEmployees(newEmployees);
+
             setEmployees(response.employeesItem);
 
             response.employeesItem.forEach(el => {
@@ -36,6 +55,19 @@ export const ClientReportPage = () => {
         loadMessage();
     }, [])
 
+    const changeHandler = event => {
+        setCurrentMonth({...currentMonth, [event.target.name]: event.target.value});
+        let newEmployees = [];
+        console.log('.slice(0, -1)', event.target.value.slice(0, -3))
+        employees.forEach((a) => {
+            if(event.target.value.slice(0, -3) === a.job.startDate.slice(0, -3)) {
+                newEmployees.push(a);
+                setEmployeesRender(newEmployees);
+            }
+        })
+        console.log('setEmployeesRender', employeesRender)
+    }
+
     if (!isLoaded) {
         return <div>Загрузка...</div>;
     } else {
@@ -44,6 +76,21 @@ export const ClientReportPage = () => {
                 <h1>
                     Client Report Page
                 </h1>
+                <label>?</label>
+                <select
+                    className="browser-default"
+                    defaultValue={data.client || ""}
+                    name="date"
+                    onChange={changeHandler}
+                >
+                <option value='Choose your option' disabled>Choose your option</option>
+                {month.map((el, index) => {
+                    return (
+                        <option key={index} value={el}>{el}</option>
+                    )
+                })}
+                </select>
+
                 <table>
                     <thead>
                         <tr>
@@ -56,7 +103,8 @@ export const ClientReportPage = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {employees.map((item, index) => {
+
+                        {employeesRender.map((item, index) => {
                             if(item.job.additionalEquipment.length == 0) {
                                 return( <tr key={index}>
                                     <td>{item.worksites.name}</td>
